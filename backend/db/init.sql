6374118e2d46c81f7e0314ca218dffc849ea1464
SanariSan <SanariSan@users.noreply.github.com>
2022-10-18 04:07:08 +0300

    feat: init sql, erd

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,59 +1,107 @@
--- REPLACE WITH NEW TABLES
-
+-- drop table IF EXISTS System_User_System_Group;
+-- drop table IF EXISTS System_Group;
+-- -- 
+-- drop table IF EXISTS Search_Query_Vacancy;
+-- drop table IF EXISTS Search_Query;
+-- drop table IF EXISTS Vacancy;
+-- --
+-- drop table IF EXISTS Token;
+-- drop table IF EXISTS System_User;
+-- --
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- 
+CREATE OR REPLACE FUNCTION upd_timestamp() RETURNS TRIGGER LANGUAGE plpgsql AS $$ BEGIN NEW.modified = CURRENT_TIMESTAMP;
+RETURN NEW;
+END;
+$$;
 -- 
--- 
--- 
--- 
--- 
-
--- DROP DATABASE IF EXISTS ts_backend_db;
--- CREATE DATABASE ts_backend_db OWNER ts_backend_admin;
-
-CREATE TABLE IF NOT EXISTS SystemUser (
-  Id        serial,
-  Login     varchar(64) NOT NULL,
-  Password  varchar(64) NOT NULL,
-  FullName  varchar(255)
+CREATE TABLE IF NOT EXISTS System_User (
+  id serial,
+  uuid_ uuid NOT NULL DEFAULT uuid_generate_v4(),
+  login varchar(64) UNIQUE NOT NULL,
+  password varchar(64) NOT NULL,
+  telegramId varchar(64),
+  hhToken varchar(255),
+  createdOn timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
-
-ALTER TABLE SystemUser ADD CONSTRAINT pkSystemUser PRIMARY KEY (Id);
-
-CREATE UNIQUE INDEX IF NOT EXISTS akSystemUserLogin ON SystemUser (Login);
-
+CREATE TRIGGER modifiedUpd BEFORE
+UPDATE ON System_User FOR EACH ROW EXECUTE PROCEDURE upd_timestamp();
 -- 
-
-CREATE TABLE IF NOT EXISTS SystemGroup (
-  Id    serial,
-  Name  varchar(64) NOT NULL
+CREATE TABLE IF NOT EXISTS System_Group (
+  id serial,
+  name varchar(64) UNIQUE NOT NULL,
+  PRIMARY KEY (id)
 );
-
-ALTER TABLE SystemGroup ADD CONSTRAINT pkSystemGroup PRIMARY KEY (Id);
-
-CREATE UNIQUE INDEX IF NOT EXISTS akSystemGroupName ON SystemGroup (Name);
-
 -- 
-
-CREATE TABLE IF NOT EXISTS GroupUser (
-  GroupId  integer NOT NULL,
-  UserId   integer NOT NULL
+CREATE TABLE IF NOT EXISTS System_User_System_Group (
+  userId integer NOT NULL,
+  groupId integer NOT NULL,
+  PRIMARY KEY (userId, groupId),
+  FOREIGN KEY (userId) REFERENCES System_User (id) ON DELETE CASCADE,
+  FOREIGN KEY (groupId) REFERENCES System_Group (id) ON DELETE CASCADE
 );
-
-ALTER TABLE GroupUser ADD CONSTRAINT pkGroupUser PRIMARY KEY (GroupId, UserId);
-ALTER TABLE GroupUser ADD CONSTRAINT fkGroupUserGroupId FOREIGN KEY (GroupId) REFERENCES SystemGroup (Id) ON DELETE CASCADE;
-ALTER TABLE GroupUser ADD CONSTRAINT fkGroupUserUserId FOREIGN KEY (UserId) REFERENCES SystemUser (Id) ON DELETE CASCADE;
-
 -- 
-
 CREATE TABLE IF NOT EXISTS Token (
-    Id serial,
-    UserId integer NOT NULL,
-    accessSignature varchar(255) NOT NULL,
-    refreshSignature varchar(255) NOT NULL
+  id serial,
+  userId integer NOT NULL,
+  accessSignature varchar(255) UNIQUE NOT NULL,
+  refreshSignature varchar(255) UNIQUE NOT NULL,
+  modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (userId) REFERENCES System_User (id) ON DELETE CASCADE
 );
-
-ALTER TABLE Token ADD CONSTRAINT pkToken PRIMARY KEY (Id);
-ALTER TABLE Token ADD CONSTRAINT fkTokensUserId FOREIGN KEY (UserId) REFERENCES SystemUser (Id) ON DELETE CASCADE;
-
-CREATE UNIQUE INDEX IF NOT EXISTS akAccessSignature ON Token (accessSignature);
-CREATE UNIQUE INDEX IF NOT EXISTS akRefreshSignature ON Token (refreshSignature);
+CREATE TRIGGER modifiedUpd BEFORE
+UPDATE ON Token FOR EACH ROW EXECUTE PROCEDURE upd_timestamp();
+-- 
+CREATE TABLE IF NOT EXISTS Search_Query (
+  id serial,
+  userId integer NOT NULL,
+  searchText varchar(255) NOT NULL,
+  searchParams varchar(255) NOT NULL,
+  isAknowledged boolean NOT NULL DEFAULT false,
+  lastAknowledged timestamp DEFAULT CURRENT_TIMESTAMP,
+  notifyPostedHoursAgoMax int DEFAULT 96,
+  uuid_ uuid NOT NULL DEFAULT uuid_generate_v4(),
+  modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (userId) REFERENCES System_User (id) ON DELETE CASCADE
+);
+CREATE TRIGGER modifiedUpd BEFORE
+UPDATE ON Search_Query FOR EACH ROW EXECUTE PROCEDURE upd_timestamp();
+-- 
+CREATE TABLE IF NOT EXISTS Vacancy (
+  id serial,
+  hash uuid UNIQUE NOT NULL,
+  vacancyHHId integer NOT NULL,
+  title varchar(64) NOT NULL,
+  snippetRequirement varchar(64) DEFAULT '',
+  snippetResponsibility varchar(64) DEFAULT '',
+  hasTest boolean NOT NULL,
+  responseLetterRequired boolean NOT NULL,
+  publishedAt varchar(64) NOT NULL,
+  createdAt varchar(64) NOT NULL,
+  areaId integer NOT NULL,
+  areaName varchar(64) NOT NULL,
+  salaryCurrencyId integer NOT NULL,
+  salaryFrom integer DEFAULT 0,
+  salaryTo integer DEFAULT 0,
+  addressRaw varchar(64) DEFAULT '',
+  scheduleId varchar(64) NOT NULL,
+  scheduleName varchar(64) NOT NULL,
+  employerName varchar(64) NOT NULL,
+  employerUrl varchar(64) NOT NULL,
+  modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+CREATE TRIGGER modifiedUpd BEFORE
+UPDATE ON Vacancy FOR EACH ROW EXECUTE PROCEDURE upd_timestamp();
+-- 
+CREATE TABLE IF NOT EXISTS Search_Query_Vacancy (
+  searchQueryId integer NOT NULL,
+  vacancyId integer NOT NULL,
+  PRIMARY KEY (searchQueryId, vacancyId),
+  FOREIGN KEY (searchQueryId) REFERENCES Search_Query (id) ON DELETE CASCADE,
+  FOREIGN KEY (vacancyId) REFERENCES Vacancy (id) ON DELETE CASCADE
+);
