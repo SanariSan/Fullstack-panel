5bf010820b2451600e7c23f6a607bb5768386979
SanariSan <SanariSan@users.noreply.github.com>
2023-04-16 18:40:51 +0400

    fix: deploy

--- frontend/src/components/landing/landing.tsx
+++ frontend/src/components/landing/landing.tsx
@@ -1,44 +1,21 @@
 import classNames from 'classnames';
 import type { FC } from 'react';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import { changeRoute } from '../../containers/history-catcher';
 import style from './landing.module.scss';
 import type { TLanding } from './landing.type';
 
 const LandingComponent: FC<TLanding> = ({ theme }) => (
-  <Container fluid className={classNames('h-100 d-flex align-items-start', style[theme])}>
+  // const [d, setD] = useState();
+
+  <Container
+    fluid
+    className={classNames('h-100 d-flex align-items-center justify-content-center', style[theme])}
+  >
     <Row className="w-100 mt-4">
-      <Col xs={12} className="d-flex justify-content-end">
-        <Button
-          className="me-2"
-          onClick={() => {
-            changeRoute('/login');
-          }}
-        >
-          login
-        </Button>
-        <Button
-          className="ms-2 me-2"
-          onClick={() => {
-            changeRoute('/register');
-          }}
-        >
-          register
-        </Button>
-        <Button
-          className="ms-2"
-          onClick={() => {
-            changeRoute('/dashboard');
-          }}
-        >
-          dashboard
-        </Button>
-      </Col>
+      <Col xs={12} className="d-flex justify-content-center"></Col>
     </Row>
   </Container>
 );
-
 export { LandingComponent };
